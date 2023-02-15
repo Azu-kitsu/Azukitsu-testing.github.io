04512fae689edf77ce20aba62e3546092eff9cca
Azu <96851229+Azu-kitsu@users.noreply.github.com>
2023-02-15 11:54:24 +0100

    switched to pngs, added zoom slider

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 get = id => document.getElementById(id);
 
-function getPapers(src, number, target) {
+function getPapers(src, number, target, filetype, plus) {
     var load = "";
     for (let i = 1; i <= number; i += 1) {
-        load += `<img src="${src}-${i}.svg">`;
+        load += `<img src="${src}${i}.${filetype}" alt="oldal ${i}" class="oldal">${plus}`;
     }
     get(target).innerHTML += load;
 }
-getPapers("./31é1sz/ÚjSágvári - 31. évfolyam 1. szám (17)", 32, "paperView");
-get("paperView").innerHTML += '<img src="./31é1sz/ÚjSágvári - 31. évfolyam 1. szám (17)-1.svg">';
+getPapers("./31e1szpng/ÚjSágvári - 31. évfolyam 1. szám (17)-", 32, "paperView", "png", "<br>");
+
+var zoom = get("zoomSlider");
+var zoomValue = get("output");
+zoomValue.innerHTML = zoom.value;
+zoom.oninput = function() {
+    output.innerHTML = this.value
+    document.querySelectorAll(".oldal").forEach(img => img.style.width = `${this.value}%`)
+}
